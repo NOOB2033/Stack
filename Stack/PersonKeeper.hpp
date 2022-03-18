80192fc6200f4307c6f8fe1a5929657f405b48b9
NOOB2033 <zaza322228@mail.ru>
2022-03-18 20:40:56 +0700

    redesigne PersonKeeper

--- Stack/PersonKeeper.hpp
+++ Stack/PersonKeeper.hpp
@@ -17,9 +17,8 @@ public:
 	using container = stack<Person>;
 
 	static PersonKeeper& instance();
-	const stack<Person>& readPersons(const std::string& filename); /* Записываем из файла в стек и возвращаем стек */
-	void writePersons(const container& stack, const std::string& filename) const; /* Записываем входящего из стека в файл */
-	//void writePersons(const stack<Person>& stack);
+	stack<Person> readPersons(std::fstream& fstream) const; /* Записываем из файла в стек и возвращаем стек */
+	void writePersons(const container& stack, std::fstream& fstream) const; /* Записываем входящего из стека в файл */
 private:
 
 	PersonKeeper() = default;
@@ -29,8 +28,6 @@ private:
 	PersonKeeper(Person&& oth) = delete;
 	PersonKeeper& operator=(const PersonKeeper& oth) = delete;
 	PersonKeeper& operator=(PersonKeeper&& oth) = delete;
-
-	container stack{};
 };
 
 
@@ -40,15 +37,14 @@ PersonKeeper& PersonKeeper::instance()
 	return keeper;
 }
 
-const stack<Person>& PersonKeeper::readPersons(const std::string& filename)
+stack<Person> PersonKeeper::readPersons(std::fstream& fstream) const
 {
-	std::ifstream file(filename);
-
-	if (!file.is_open()) /* Проверяем, открыли или нет */
+	if (!fstream.is_open()) /* Проверяем, открыли или нет */
 		throw std::runtime_error("File not found\n");
 
+	stack<Person> stack;
 	std::string buffer;
-	while (std::getline(file, buffer)) /* Записываем строки в буфер */
+	while (std::getline(fstream, buffer)) /* Записываем строки в буфер */
 	{
 		auto it = buffer.cbegin();
 
@@ -78,21 +74,19 @@ const stack<Person>& PersonKeeper::readPersons(const std::string& filename)
 		for (; it != buffer.cend(); ++it)
 			patronymic.push_back(*it);
 
-		stack.push(Person(std::move(last_name), std::move(first_name), std::move(patronymic))); /* Пушим в стек */
+		stack.push(Person(std::move(last_name), std::move(first_name), std::move(patronymic)));/* Пушим в стек */
 	}
 
 	return stack;
 }
 
-void PersonKeeper::writePersons(const container& stack, const std::string& filename) const
+void PersonKeeper::writePersons(const container& stack, std::fstream& fstream) const
 {
-	std::ofstream file(filename);
-
-	if (!file.is_open())
+	if (!fstream.is_open())
 		throw std::runtime_error("File not found\n");
 
 	for (auto it : stack.getContainer()) /* Записываем в стек из контейнера */
-		file << it.getLastName() + ' ' + it.getFirstName() + ' ' + it.getPatronymic() + '\n';
+		fstream << it.getLastName() + ' ' + it.getFirstName() + ' ' + it.getPatronymic() + '\n';
 }
 
 
